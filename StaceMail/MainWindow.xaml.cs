19e5a33c04a76f5fc93b799c17d1e3f5811b201b
Mark Archer <marcher@uci.edu>
2014-09-19 18:43:55 -0700

    savepoint

--- StaceMail/MainWindow.xaml.cs
+++ StaceMail/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using SendGrid;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,5 +27,50 @@ namespace StaceMail
         {
             InitializeComponent();
         }
+
+        private void SendEmails_Click(object sender, RoutedEventArgs e)
+        {
+            var body = @"
+another test
+
+On Fri, Sep 19, 2014 at 6:05 PM, Mark Archer <mmadink@gmail.com> wrote:
+test
+
+On Fri, Sep 19, 2014 at 6:04 PM, <mmadink@gmail.com> wrote:
+test
+test 
+
+";
+
+            SendEmail(
+                from: "mmadink@gmail.com", 
+                to: new String[]{"mark_archer@live.com"}, 
+                subject: "Re: StaceMail", 
+                body: body);
+        }
+
+        private void SendEmail(string from, string[] to, string subject, string body, bool html_body = true)
+        {            
+            // Create an email, passing in the the eight properties as arguments.
+            var email = new SendGridMessage();
+            email.From = new MailAddress(from);
+            email.To = to.Select(s => new MailAddress(s)).ToArray();
+            email.Subject = subject;
+            if (html_body)
+                email.Html = body;
+            else
+                email.Text = body;
+            
+            SendEmail(email);
+        }
+
+        public static void SendEmail(SendGridMessage myMessage)
+        {
+            var credentials = new NetworkCredential("mmadink@gmail.com", "Echilon3");
+            var transportWeb = new Web(credentials);
+            transportWeb.Deliver(myMessage);            
+        }
+
+        
     }
 }
