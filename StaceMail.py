30d5e76f955a579294815c2fc577cfc5d31d8712
Mark Archer <marcher@uci.edu>
2014-09-20 13:42:47 -0700

    correctly sending replies

--- StaceMail.py
+++ StaceMail.py
@@ -3,11 +3,13 @@
 import httplib2 
 from email.mime.text import MIMEText
 import base64
+import datetime
 
 from apiclient.discovery import build
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import run
+from apiclient import errors
 
 
 def authenticate():
@@ -46,38 +48,88 @@ def get_threads(gmail_service, subject):
 
 	return threads
 
-def create_message(sender, to, subject, message_text, thread_id=None):
+def get_thread(service, user_id, thread_id):
+  try:
+    thread = service.users().threads().get(userId=user_id, id=thread_id).execute()
+    messages = thread['messages']    
+    return thread
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
+
+def create_message(sender, to, subject, message_text, thread_id=None, rpl_msg_id_raw=None):
 	message = MIMEText(message_text)
 	message['to'] = to
 	message['from'] = sender
 	message['subject'] = subject
-	if thread_id:
-		message['threadId'] = thread_id
-	return {'raw': base64.urlsafe_b64encode(message.as_string())}
+	#message['message-id'] = "%s@stacemail" % datetime.datetime.now().isoformat()
+	raw_msg = {}
+	if thread_id is not None and rpl_msg_id_raw is not None:
+		message['In-Reply-To'] = rpl_msg_id_raw
+		message['References'] = rpl_msg_id_raw
+		message['subject'] = 'Re: ' + subject
+		raw_msg['threadId'] = thread_id
+	raw_msg['raw'] = base64.urlsafe_b64encode(message.as_string())
+	return raw_msg
 
 def create_draft(service, user_id, message_body):
   try:
     message = {'message': message_body}
     draft = service.users().drafts().create(userId=user_id, body=message).execute()
 
-    print 'Draft id: %s\nDraft message: %s' % (draft['id'], draft['message'])
+    #print 'Draft id: %s\nDraft message: %s' % (draft['id'], draft['message'])
 
     return draft
   except errors.HttpError, error:
     print 'An error occurred: %s' % error
     return None
 
-#def create_drafts(gmail_service, threads):
-	
+def send_message(service, user_id, message_body):
+	try:
+		message = (service.users().messages().send(userId=user_id, body=message_body).execute())
+		print 'sent Message Id: %s' % message['id']
+		return message
+	except errors.HttpError, error:
+		print 'An error occurred: %s' % error
+
 gmail_service = authenticate()
-threads = get_threads(gmail_service, 'stacemail')
+threads = get_threads(gmail_service, 'StaceMail')
+
+to = 'marcher@srcity.org'
+if "threads" not in threads.keys():
+	aMsg = create_message('mmadink@gmail.com',to,'StaceMail', 'test with python')
+	send_message(gmail_service, 'me', aMsg)
 
 # Print ID for each thread
-if threads['threads']:
-  for thread in threads['threads']:
-    print thread.keys()
-    #print 'Thread ID: %s; To: %s' % (thread['id'], thread['to'])
+else:
+	for thread in threads['threads']:
+		#print thread.keys()
+		thread_id = thread['id']
+		
+		tthread = get_thread(gmail_service,'me',thread_id)
+		messages = tthread['messages']
+		msg_count = len(messages)		
+		last_msg = messages[msg_count-1]
+		msg_id_raw = None
+		for header in last_msg['payload']['headers']:
+			if header['name'].lower() == 'message-id':
+				msg_id_raw = header['value']
+		if msg_id_raw is None:
+			print "Couldn't reply to thread: %s, Message-id is None" % (thread_id)
+			continue
 
-aMsg = create_message('mmadink@gmail.com','mmadink@gmail.com','stacemail', 'test with python')
-create_draft(gmail_service, 'me', aMsg)
+		print "thread id: %s" % thread_id
+		print "message id: %s" % msg_id_raw
+		print "msg_count: %s" % msg_count
+
+		aMsg = create_message('mmadink@gmail.com',to,'StaceMail', 'test with python', thread_id, msg_id_raw)
+		#create_draft(gmail_service, 'me', aMsg)
+		send_message(gmail_service, 'me', aMsg)
+		print ''
+		#break;
+
+
+
+
+#print threads['threads'][0]['id']
+
 
